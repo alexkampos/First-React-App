bc88bbc017a4619dcff64b54f7e7fc5568af7f61
Alex <alexkambos@gmail.com>
2020-09-04 11:29:54 +0300

    Updates

---
+++ precache-manifest.e816bf639f757ff258b4b49857ce6bb0.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d7b5f672c19c2db1725610765602f88",
+    "url": "/First-React-App/index.html"
+  },
+  {
+    "revision": "08cd8415ba9542f142a7",
+    "url": "/First-React-App/static/css/2.803429d5.chunk.css"
+  },
+  {
+    "revision": "8dc867698b2d091e2578",
+    "url": "/First-React-App/static/css/main.52bcba44.chunk.css"
+  },
+  {
+    "revision": "08cd8415ba9542f142a7",
+    "url": "/First-React-App/static/js/2.25c846ea.chunk.js"
+  },
+  {
+    "revision": "0808c75b50af43a39fe893124e2f5e72",
+    "url": "/First-React-App/static/js/2.25c846ea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8dc867698b2d091e2578",
+    "url": "/First-React-App/static/js/main.0ab61fc0.chunk.js"
+  },
+  {
+    "revision": "63c6efb92ea66b2d3fc9",
+    "url": "/First-React-App/static/js/runtime-main.d3f9f6b8.js"
+  },
+  {
+    "revision": "3a68c32c9f20a6db97ccb04a6d00a9f5",
+    "url": "/First-React-App/static/media/calendar.3a68c32c.png"
+  },
+  {
+    "revision": "77162ab81cfc8b2af83fe35cfa36034c",
+    "url": "/First-React-App/static/media/heart.77162ab8.png"
+  },
+  {
+    "revision": "fe78a05f3e7bf59f9607bcdd29a8058d",
+    "url": "/First-React-App/static/media/map.fe78a05f.jpg"
+  },
+  {
+    "revision": "878c7769f3435fc9f2625967b25129cf",
+    "url": "/First-React-App/static/media/search.878c7769.png"
+  },
+  {
+    "revision": "6f4430c6e986ebfbc65e000cfe969c23",
+    "url": "/First-React-App/static/media/star.6f4430c6.png"
+  }
+]);
